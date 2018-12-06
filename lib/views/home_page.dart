8d6f41f7c6d0d04fca91b0a1980db5a1efa9bc52
Aurghyadip <adkundu@gmail.com>
2018-12-06 11:57:51 +0530

    implemented smaple dashboard with working data fetch

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-import 'login_page.dart';
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -11,24 +9,71 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
+  Future<FirebaseUser> _getCurrentUser() async {
+    FirebaseUser _firebaseUser;
+    if (_firebaseAuth.currentUser() != null) {
+      _firebaseUser = await _firebaseAuth.currentUser();
+      return _firebaseUser;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Container(
-          color: Colors.green,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Dashboard',
+          style: TextStyle(color: Colors.black, fontSize: 20.0),
         ),
-        Center(
-          child: FlatButton(
-            onPressed: () {
-              _firebaseAuth.signOut();
-            },
-            child: Text("Logout"),
-            textColor: Colors.white,
-            color: Colors.black,
-          )
-        )
-      ],
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: FutureBuilder(
+        future: _getCurrentUser(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return Container(
+              child: Text("Loading..."),
+            );
+          } else {
+            //print(snapshot.data);
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 150.0,
+                    height: 150.0,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: new Border.all(
+                            color: Colors.blueAccent.shade400, width: 6.0),
+                        image: new DecorationImage(
+                            fit: BoxFit.fill,
+                            image: NetworkImage(snapshot.data.photoUrl))),
+                  ),
+                  SizedBox(height: 10.0),
+                  Text(
+                    snapshot.data.displayName,
+                    style:
+                        TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10.0),
+                    child: Text(
+                      snapshot.data.email,
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16.0
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
